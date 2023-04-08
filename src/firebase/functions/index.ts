c58c32a5debb2b4c9775f5223e3a54a385e0a8e2
nelsonortega <neor9712@gmail.com>
2023-04-07 23:42:12 -0600

    Added linting

--- src/firebase/functions/index.ts
+++
@@ -1,2 +0,0 @@
-export { createDocument, updateDocument, getAllDocuments } from './FirestoreHandlers'
-export { loginUser, logoutUser, registerUser, isUserAdmin } from './user'
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,2 +1,3 @@
-export * from './functions'
 export { firestoreDB, firestoreStorage, firebaseAuth } from './firebase'
+export { loginUser, logoutUser, registerUser, isUserAdmin } from './functions/user'
+export { createDocument, updateDocument, getAllDocuments } from './functions/firestoreHandlers'
